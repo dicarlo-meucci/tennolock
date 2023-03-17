bfd3c833db6c49d8d41bcc61dbb086a54ddb978f
iQuick <iquickpublic@gmail.com>
2023-03-18 00:07:36 +0100

    added footer + edited header

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector("#copyright-year").textContent = new Date().getFullYear()
+
+let goToTopBtn = document.querySelector("#go-to-top")
+
+goToTopBtn.onclick = () => {
+    window.scrollTo(0, 0)
+}
+
+window.onscroll = () =>
+{
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight)
+    {
+        goToTopBtn.classList.remove("hidden")
+    }
+    else
+    {
+        goToTopBtn.classList.add("hidden")
+    }
+}
