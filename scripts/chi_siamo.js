cda8952f82f062dfbe64a63cfcfb9e8f3ee243d7
MerattiGiacomo <merattigiacomo@gmail.com>
2023-03-24 13:18:08 +0100

    new animation fadein, fadeout doesnt work

--- scripts/chi_siamo.js
+++ scripts/chi_siamo.js
@@ -1,4 +1,7 @@
 function eventChiSiamo(){
     let text = document.querySelector(".chi-siamo-text")
+    text.classList.toggle("fadeout")
     text.hidden = !text.hasAttribute("hidden")
+    text.classList.toggle("fadein")
+    
 }
