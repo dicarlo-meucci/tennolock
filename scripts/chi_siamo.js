dedee371b3d403639734faceaeded175aaad321d
MerattiGiacomo <merattigiacomo@gmail.com>
2023-03-25 08:41:20 +0100

    chi siamo modified, button eliminater, added wave

--- scripts/chi_siamo.js
+++
@@ -1,7 +0,0 @@
-function eventChiSiamo(){
-    let text = document.querySelector(".chi-siamo-text")
-    text.classList.toggle("fadeout")
-    text.hidden = !text.hasAttribute("hidden")
-    text.classList.toggle("fadein")
-    
-}
