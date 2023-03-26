2f350477e340fa50b75cfa3b68f84d7c7c8a6946
iQuick <iquickpublic@gmail.com>
2023-03-26 17:07:58 +0200

    fixed some things, removed powered by
    Co-authored-by: MerattiGiacomo <MerattiGiacomo@users.noreply.github.com>

---
+++ scripts/remove_translate.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    
+    let removeTranslateInterval = setInterval(() => {
+        if (!checkTranslateText())
+        {
+            clearInterval(removeTranslateInterval)
+            removeTranslateText()
+            console.log("tried removing")
+        }
+    }, 50)
+    removeTranslateText()
+    
+}
+
+function checkTranslateText() {
+    return document.evaluate("/html/body/div[1]/header/div/div/div/text()", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+}
+
+function removeTranslateText() {
+    document.evaluate("/html/body/div[1]/header/div/div/div/text()", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.remove()
+}
